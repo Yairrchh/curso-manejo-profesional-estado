fc21ecd56f57a2ea272b514e43a9b6607cb3677c
unknown <chirayair@gmail.com>
2023-09-06 22:17:22 -0400

    Semi-declarative states with useState

--- src/UseState.js
+++ src/UseState.js
@@ -17,6 +17,35 @@ function UseState({ name }) {
 
     console.log(state)
 
+    const onConfirm = () => {
+        setState({...state, loading: false, error: false, confirmed: true})
+    }
+
+    const onError = () => {
+        setState({...state, error: true, loading:false})
+    }
+
+    const onWrite = (newValue) => {
+        setState({...state, value: newValue})
+    }
+
+    const onFindOut = () => {
+        setState({...state, loading: true})
+    }
+
+    const onDelete = () => {
+        setState({...state, delete: true})
+    }
+
+    //Because it has almost the same functionality as the onRestore we are going to use a single function to have less code
+    // const onNoDelete = () => {
+    //     setState({...state, confirmed: false, value: ''})
+    // }
+
+    const onRestore = () => {
+        setState({...state, confirmed: false, delete: false, value: ''})
+    }
+
     React.useEffect(() =>{
         console.log('start effect')
 
@@ -25,9 +54,9 @@ function UseState({ name }) {
                 console.log('start validation')
 
                 if(state.value === SECURITY_CODE) {
-                    setState({...state, loading: false, error: false, confirmed: true})
+                    onConfirm();
                 } else {
-                    setState({...state, error: true, loading:false})
+                    onError()
                 }
 
                 console.log('end validation')
@@ -55,12 +84,12 @@ function UseState({ name }) {
                 <input placeholder="code of security"
                 value={state.value}
                 onChange={(event) => {
-                    setState({...state, value: event.target.value})
+                    onWrite(event.target.value);
                 }}
                 />
                 <button
-                    onClick={() => setState({...state, loading: true})}
-                >find out</button>
+                    onClick={() => onFindOut()}
+                >Find out</button>
             </div>
             )
     } else if(state.confirmed && !state.delete) {
@@ -68,10 +97,10 @@ function UseState({ name }) {
             <React.Fragment>
                 <p>Are you sure you want to delete?</p>
                 <button onClick={() => {
-                    setState({...state, delete: true})
+                    onDelete();
                 }}>Yes, Delete</button>
                 <button onClick={() => {
-                    setState({...state, confirmed: false})
+                    onRestore();
                 }}>No, go back</button>
             </React.Fragment>
         )
@@ -80,7 +109,7 @@ function UseState({ name }) {
             <React.Fragment>
                 <p>Delete Successfully</p>
             <button onClick={() => {
-                setState({...state, confirmed: false, delete: false, value: ''})
+                onRestore();
             }}>Retrieve useState</button>
             </React.Fragment>
         )
